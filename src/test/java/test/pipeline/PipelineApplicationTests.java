dc5059fbd121cade267fab714ee0166384f0ad19
TopWoman <top.woman.in.the.mai@gmail.com>
2024-11-18 16:03:47 +0300

    fix

--- src/test/java/test/pipeline/PipelineApplicationTests.java
+++ src/test/java/test/pipeline/PipelineApplicationTests.java
@@ -1,5 +1,7 @@
 package test.pipeline;
 
+import static io.restassured.RestAssured.given;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PipelineApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    public void testGetPost() {
+        RestAssured.baseURI = "https://httpbin.org/#/HTTP_Methods";
 
+        given()
+                .when()
+                .get("/get")
+                .then()
+                .statusCode(200);
+    }
 }
